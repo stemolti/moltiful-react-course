b4e3eedffb66d311a7acd851c994b822b9035993
stemolti <mystefano95@gmail.com>
2024-05-25 07:29:41 +0200

    Completed JS Refresh

---
+++ codes/02-js-refresher/assets/scripts/app.js
@@ -0,0 +1,29 @@
+// EXPORT IMPORT TYPES
+/* 
+import first from './utils.js';
+import {apiKey} from './utils.js';
+import * as utils from './utils.js';
+import {apikey, abc as content } from './utils.js'; 
+*/
+
+// console.log(util.apiKey);
+// console.log(content);
+
+//console.log('Hello STEY!');
+
+/* 
+function combine (a, b, c){
+  return a * b / c
+}
+
+console.log(combine(6,5,5)); */
+
+
+/* function transformToObjects(numbersArray){
+  const editedObjArray = numbersArray.map(num => ({
+    val: num
+  }));
+return editedObjArray; 
+}
+
+console.log(transformToObjects([1, 2, 3])); */
---
+++ codes/02-js-refresher/assets/scripts/utils.js
@@ -0,0 +1,6 @@
+// EXPORT IMPORT TYPES
+/* 
+export default 'asddsffnvdfvldf';
+export let apiKey = 'ghrgryhwergerge';
+export let abc = 'abc'; 
+*/
